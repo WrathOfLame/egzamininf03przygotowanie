9cd788d2a4c7757892e6093fa6eed2bc570388f9
WrathOfLame <118675328+WrathOfLame@users.noreply.github.com>
2026-04-20 13:32:52 +0200

    Add zlecenia.php with initial HTML structure

---
+++ zlecenia.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Remonty</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header><h1>Malowanie i gipsowanie</h1></header>
+    <main>
+        <section class ="blok_lewy">
+            <section class ="blok_nawigacyjny">
+                <nav class="navblok1">
+                    <a href="kontakt.html">Kontakt</a>
+                    <a href="remonty.pl" target = "_blank">Partnerzy</a>
+                </nav>
+                <nav class ="navblok2"></nav>
+            </section>
+            <h2>Dla klientów</h2>
+            <form class = 'blok1' method="post">
+                <p>Ilu conajmniej potrzebujesz?</p><br>
+                <input class = "input1" type="text">
+                <button type="submit">Szukaj film</button>
+            </form>
+            <h2>Dla wykonawców</h2>
+            <form class="blok2" method="post">
+                <select class = 'wybor'>
+                </select>
+                <input type="radio" name="typ_pracy" selected>
+                <input type="radio" name="typ_pracy">
+                <button type="submit">Szukaj klientów</button>
+            </form>
+            <ul></ul>
+        </section>
+        <section class ="sekcja_boczna">
+            <img src="" alt="usługi">
+            <img src="" alt="usługi">
+            <img src="" alt="usługi">
+        </section>
+    </main>
+    <footer>
+        <p><strong>Numer zdającego: </strong></p>
+    </footer>
+</body>
+</html>
